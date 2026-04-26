43cf771d41d9a61f70c03326b2bde3d9f1a9e7cd
tmsk01 <tomas.stankevic@ovoko.com>
2026-04-26 15:03:11 +0300

    Update OSDCloud_Start.ps1

--- OSDCloud_Start.ps1
+++ OSDCloud_Start.ps1
@@ -1,13 +1,11 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
    Start-Sleep -Seconds 5
 
-   # Make sure I have the latest OSD Content
    Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
    Install-Module OSD -Force
 
    Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
    Import-Module OSD -Force
 
-   # Start OSDPad - points at YOUR repo's ScriptPad folder
    Write-Host -ForegroundColor Green "Starting OSDPad"
    Start-OSDPad -RepoOwner tmsk01 -RepoName rrr-osd -RepoFolder ScriptPad -Hide Script -BrandingTitle 'RRR Windows Deployment'
